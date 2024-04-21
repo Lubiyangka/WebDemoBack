1539cac33470e32ab2e1e96d8358000b4c7c2789
John Six <1424157706@qq.com>
2024-04-21 23:28:13 +0800

    添加Spring Security + JWT配置

---
+++ src/main/java/com/webdemo/utils/JWTUtils.java
@@ -0,0 +1,14 @@
+package com.webdemo.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+@Data
+public class JWTUtils {
+
+}
